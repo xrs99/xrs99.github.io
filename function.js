29f13423bd33557a5a415873605e4bd7d2a22ed2
rileyts <rileys797@gmail.com>
2021-04-22 16:55:54 -1000

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
 function myfunction() {   
-    var x = document.createElement("IMG");
-    x.setAttribute("src", "images/turtwig-test.jpg");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "Test Image");
-    document.body.appendChild(x);
+    //var x = document.createElement("IMG");
+    //x.setAttribute("src", "images/turtwig-test.jpg");
+    //x.setAttribute("width", "304");
+    //x.setAttribute("height", "228");
+    //x.setAttribute("alt", "Test Image");
+    //document.body.appendChild(x);
+    alert("Hi");
 }  
