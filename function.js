cf3d5eb22d692a1d4f65cc157656620d3b4ba401
rileyts <rileys797@gmail.com>
2021-04-22 16:36:30 -1000

    fix

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
 function myfunction() {   
-    var img = "images/turtwig-test.jpg";
-    img.style.display = "block"; 
+    var sourceOfPicture = "http://img.tesco.com/Groceries/pi/118/5000175411118/IDShot_90x90.jpg";
+    var img = document.getElementById('bigpic')
+    img.src = sourceOfPicture.replace('90x90', '225x225');
+    img.style.display = "block";
 }  
