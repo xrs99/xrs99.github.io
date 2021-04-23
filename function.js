2574057910e648994bd2b6427739030fe4225c92
rileyts <rileys797@gmail.com>
2021-04-22 17:09:46 -1000

    fix

--- function.js
+++ function.js
@@ -1,9 +1,12 @@
-function myfunction() {   
+//function myfunction() {   
     //var x = document.createElement("IMG");
     //x.setAttribute("src", "images/turtwig-test.jpg");
     //x.setAttribute("width", "304");
     //x.setAttribute("height", "228");
     //x.setAttribute("alt", "Test Image");
     //document.body.appendChild(x);
-    alarm("Hi!");
+//}  
+
+function myfunction() {   
+    alert("how are you");  
 }  
