6a7f2a0889b9f1c821c40b2814c05ce0dddef4bc
rileyts <rileys797@gmail.com>
2021-04-22 16:35:02 -1000

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,4 @@
 function myfunction() {   
-    var sourceOfPicture = "images/turtwig-test.jpg";
-    var img = document.getElementById('bigpic')
-    img.src = sourceOfPicture.replace('90x90', '225x225');
+    var img = "images/turtwig-test.jpg";
     img.style.display = "block"; 
 }  
